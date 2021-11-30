79531fef17b337cc586cf77c1ee11592fed32fbe
andy- Mac <andrew.zhou93@gmail.com>
2021-11-29 17:50:21 -0800

    fix: add deployment amazon config

--- weather/index.js
+++ weather/index.js
@@ -6,12 +6,20 @@ import fetch from "node-fetch";
 const router = express.Router();
 
 const fetchSpaceWeather = async () => {
+  // End Date == Today!
+  // Testing Historical Weather Events
+
+  // const testEndDate = "2021-7-17";
+  // const testStartDate = "2021-6-5";
+  // const testEndpoint = `https://api.nasa.gov/DONKI/notifications?startDate=${testStartDate}&endDate=${testEndDate}&type=all&api_key=${API_KEY}`;
+
   const today = new Date().toISOString();
   const endpoint = `https://api.nasa.gov/DONKI/notifications?startDate=YYYY-MM-DD&endDate=${today}&type=all&api_key=${process.env.SPACE_WEATHER_API_KEY}`;
 
   try {
     const weatherStream = await fetch(endpoint);
     const weatherJSON = await weatherStream.json();
+    // console.log("testEndDate: ", testDate);
     return weatherJSON;
   } catch (err) {
     return { Error: err.stack };
