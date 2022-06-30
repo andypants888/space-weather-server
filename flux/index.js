5a4d4cae1a878dcadd82be0dc63410e9d1bacad4
andy- Mac <andrew.zhou93@gmail.com>
2022-06-29 17:04:56 -0700

    access mongodb flux

--- flux/index.js
+++ flux/index.js
@@ -1,11 +1,51 @@
 import express from "express";
-import fetch from "node-fetch";
-
+import { MongoClient, ServerApiVersion } from "mongodb";
 const router = express.Router();
 
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+async function main() {
+  const uri = `mongodb+srv://spaceweather:${process.env.MONGO_PASSWORD}@cluster0.ya4xd.mongodb.net/?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    const cursor = client
+      .db("spaceWeather")
+      .collection("NOAA")
+      .find({
+        scrape_time: { $lte: new Date() },
+        // radio_flux: { $gte: 102 },
+      })
+      .sort({ scrape_time: -1 })
+      .limit(4);
+    // .find({ scrape_time: new Date("2022-01-26T07:01:20.276+00:00") });
+
+    let results = await cursor.toArray();
+    console.log(results);
+    // Make the appropriate DB calls
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+async function listDatabases(client) {
+  let databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+// main().catch(console.error);
 router.get("/", async (req, res) => {
-  const data = "flux data incoming";
-  res.json(data);
+  const testString = "flux data incoming";
+  const data = main().catch(console.error);
+  // res.json(data);
+  res.json(testString);
 });
 
 export default router;
